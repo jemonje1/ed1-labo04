1b0f335aef4be2e6fa16f2b1cc78610b1c2aaae7
javu2 <jemonje@correo.url.edu.gt>
2025-05-27 01:15:21 -0600

    Pushito con actions para el lab

---
+++ src/main/java/ed/lab/ed1labo04/entity/ProductEntity.java
@@ -0,0 +1,45 @@
+package ed.lab.ed1labo04.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private Double price;
+    private int quantity;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/ed/lab/ed1labo04/model/UpdateProductRequest.java
@@ -0,0 +1,22 @@
+package ed.lab.ed1labo04.model;
+
+public class UpdateProductRequest {
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    Double price;
+    int quantity;
+}
